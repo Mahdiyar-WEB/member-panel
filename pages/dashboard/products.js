2533d55f10e35d7236dd9fe0c55074d35f6d5f9d
mahdiyar <mahdiyar472@gmail.com>
2023-02-06 18:10:45 +0330

    configure changes to handle select products in dashboard/products

--- pages/dashboard/products.js
+++ pages/dashboard/products.js
@@ -9,86 +9,216 @@ import {
   TextField,
   Box,
 } from "@mui/material";
-import PanelProduct from "../../src/components/PanelProduct/PanelProduct";
-import product1 from "../../public/product1.jpeg";
-import product2 from "../../public/product2.jpeg";
 import Select from "@mui/material/Select";
 import { useEffect, useState } from "react";
-import { FaTelegramPlane } from "react-icons/fa";
 
 const products = [
   {
     id: 1,
-    category: "s1",
+    category: "S1",
     subCategories: [
-      { subId: 1, title: "s11" },
-      { subId: 2, title: "s12" },
-      { subId: 3, title: "s13" },
+      {
+        id: 1,
+        category: "S11",
+        description: "This is s11",
+        price: 2,
+        quantity: 0,
+        minQuantity: 0,
+        maxQuantity: 30,
+      },
+      {
+        id: 2,
+        category: "S12",
+        description: "This is s12",
+        price: 1,
+        quantity: 1,
+        minQuantity: 1,
+        maxQuantity: 30,
+      },
+      {
+        id: 3,
+        category: "S13",
+        description: "This is s13",
+        price: 0.5,
+        quantity: 2,
+        minQuantity: 2,
+        maxQuantity: 30,
+      },
+      {
+        id: 4,
+        category: "S14",
+        description: "This is s14",
+        price: 1.5,
+        quantity: 3,
+        minQuantity: 3,
+        maxQuantity: 30,
+      },
     ],
-    activeSub: 1,
-    description: "hello this is s1",
-    quantity: 100,
-    minQuantity: 100,
-    maxQuantity: 900,
+    activeSub: {
+      id: 1,
+      category: "S11",
+      description: "This is s11",
+      price: 2,
+      quantity: 0,
+      minQuantity: 0,
+      maxQuantity: 30,
+    },
   },
   {
     id: 2,
     category: "s2",
     subCategories: [
-      { subId: 1, title: "s21" },
-      { subId: 2, title: "s22" },
-      { subId: 3, title: "s23" },
+      {
+        id: 1,
+        category: "S21",
+        description: "This is s21",
+        price: 2,
+        quantity: 4,
+        minQuantity: 4,
+        maxQuantity: 30,
+      },
+      {
+        id: 2,
+        category: "S22",
+        description: "This is s22",
+        price: 1,
+        quantity: 5,
+        minQuantity: 5,
+        maxQuantity: 30,
+      },
+      {
+        id: 3,
+        category: "S23",
+        description: "This is s23",
+        price: 0.5,
+        quantity: 6,
+        minQuantity: 6,
+        maxQuantity: 30,
+      },
+      {
+        id: 4,
+        category: "S24",
+        description: "This is s24",
+        price: 1.5,
+        quantity: 7,
+        minQuantity: 7,
+        maxQuantity: 30,
+      },
     ],
-    activeSub: 1,
-    description: "hello this is s2",
-    quantity: 10,
-    minQuantity: 10,
-    maxQuantity: 30,
+    activeSub: {
+      id: 1,
+      category: "S21",
+      description: "This is s21",
+      price: 2,
+      quantity: 4,
+      minQuantity: 4,
+      maxQuantity: 30,
+    },
   },
   {
     id: 3,
     category: "s3",
     subCategories: [
-      { subId: 1, title: "s31" },
-      { subId: 2, title: "s32" },
-      { subId: 3, title: "s33" },
+      {
+        id: 1,
+        category: "S31",
+        description: "This is s31",
+        price: 2,
+        quantity: 8,
+        minQuantity: 8,
+        maxQuantity: 100,
+      },
+      {
+        id: 2,
+        category: "S32",
+        description: "This is s32",
+        price: 1,
+        quantity: 9,
+        minQuantity: 9,
+        maxQuantity: 100,
+      },
+      {
+        id: 3,
+        category: "S33",
+        description: "This is s33",
+        price: 0.5,
+        quantity: 10,
+        minQuantity: 10,
+        maxQuantity: 100,
+      },
+      {
+        id: 4,
+        category: "S34",
+        description: "This is s34",
+        price: 1.5,
+        quantity: 11,
+        minQuantity: 11,
+        maxQuantity: 100,
+      },
     ],
-    activeSub:1,
-    description: "hello this is s3",
-    quantity: 0,
-    minQuantity: 0,
-    maxQuantity: 10,
-  },
-  {
-    id: 4,
-    category: "s4",
-    subCategories: [
-      { subId: 1, title: "s41" },
-      { subId: 2, title: "s42" },
-      { subId: 3, title: "s43" },
-    ],
-    activeSub: 1,
-    description: "hello this is s4",
-    quantity: 20,
-    minQuantity: 20,
-    maxQuantity: 30,
+    activeSub: {
+      id: 1,
+      category: "S31",
+      description: "This is s31",
+      price: 2,
+      quantity: 8,
+      minQuantity: 8,
+      maxQuantity: 100,
+    },
   },
 ];
 
 const Products = () => {
   const [product, setProduct] = useState({
     id: 1,
-    category: "",
+    category: "S1",
     subCategories: [
-      { subId: 1, title: "" },
-      { subId: 2, title: "" },
-      { subId: 3, title: "" },
+      {
+        id: 1,
+        category: "S11",
+        description: "This is s11",
+        price: 2,
+        quantity: 20,
+        minQuantity: 20,
+        maxQuantity: 30,
+      },
+      {
+        id: 2,
+        category: "S12",
+        description: "This is s12",
+        price: 1,
+        quantity: 20,
+        minQuantity: 20,
+        maxQuantity: 30,
+      },
+      {
+        id: 3,
+        category: "S13",
+        description: "This is s13",
+        price: 0.5,
+        quantity: 20,
+        minQuantity: 20,
+        maxQuantity: 30,
+      },
+      {
+        id: 4,
+        category: "S14",
+        description: "This is s14",
+        price: 1.5,
+        quantity: 20,
+        minQuantity: 20,
+        maxQuantity: 30,
+      },
     ],
-    activeSub: 1,
-    description: "",
-    quantity: 0,
-    minQuantity: 0,
-    maxQuantity: 0,
+    activeSub: {
+      id: 1,
+      category: "S11",
+      description: "This is s11",
+      price: 2,
+      quantity: 20,
+      minQuantity: 20,
+      maxQuantity: 30,
+    },
   });
 
   useEffect(() => {
@@ -96,30 +226,20 @@ const Products = () => {
   }, []);
 
   const handleChange = (e) => {
-    console.log(e.target.value);
     const definedProduct = products.find(
       (product) => product.id === e.target.value
     );
     setProduct(definedProduct);
   };
   const handleSubChange = (e) => {
-    setProduct({ ...product, activeSub: e.target.value });
+    const definedProduct = product.subCategories.find(
+      (item) => item.id === e.target.value
+    );
+    setProduct({ ...product, activeSub: definedProduct });
   };
 
   return (
     <article className={`${styles.container} row`}>
-      {/* <PanelProduct
-        title="Product1"
-        text={`The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona.`}
-        image={product1}
-        price="2"
-      />
-      <PanelProduct
-        title="Product2"
-        text={`The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.`}
-        image={product2}
-        price="3"
-      /> */}
       <Box className="d-flex flex-column gap-4">
         <FormControl fullWidth>
           <InputLabel id="demo-simple-select-label">Products</InputLabel>
@@ -144,14 +264,14 @@ const Products = () => {
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={product?.activeSub}
+            value={product?.activeSub.id}
             label="Service"
             onChange={handleSubChange}
           >
             {product?.subCategories.map((subCategory) => {
               return (
-                <MenuItem key={subCategory.subId} value={subCategory.subId}>
-                  {subCategory.title}
+                <MenuItem key={subCategory.id} value={subCategory.id}>
+                  {subCategory.category}
                 </MenuItem>
               );
             })}
@@ -163,7 +283,7 @@ const Products = () => {
           label="Description"
           multiline
           rows={4}
-          value={product.description}
+          value={product.activeSub.description}
         />
         <TextField
           fullWidth
@@ -180,10 +300,18 @@ const Products = () => {
           label="Quantity"
           type="number"
           InputProps={{
-            inputProps: { min: product.minQuantity, max: product.maxQuantity },
+            inputProps: {
+              min: product.activeSub.minQuantity,
+              max: product.activeSub.maxQuantity,
+            },
           }}
-          value={product.quantity}
-          onChange={(e) => setProduct({ ...product, quantity: e.target.value })}
+          value={product.activeSub.quantity}
+          onChange={(e) =>
+            setProduct({
+              ...product,
+              activeSub: { ...product.activeSub, quantity: e.target.value },
+            })
+          }
         />
         <Button variant="contained" className="col-12 col-md-4">
           Submit
